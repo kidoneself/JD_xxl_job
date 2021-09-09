ba76cafde0acd8523109509b32bc1e102fd2a155
kidoneself <47913364+kidoneself@users.noreply.github.com>
2021-09-09 09:43:09 +0800

    init

--- xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/mvc/mapper/EvnMapper.java
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/main/java/com/xxl/job/executor/mvc/mapper/EvnMapper.java
@@ -1,21 +1,21 @@
-package com.xxl.job.executor.mvc.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.xxl.job.executor.po.Env;
-import org.jeecg.modules.tort.entity.FbmUserInfo;
-import org.springframework.web.bind.annotation.Mapping;
-
-import java.util.List;
-import java.util.Map;
-
-/**
- * @Description: 用户作者
- * @Author: jeecg-boot
- * @Date: 2021-09-06
- * @Version: V1.0
- */
-@Mapping
-public interface EvnMapper extends BaseMapper<Env> {
-
-    Map<Integer, FbmUserInfo> getByUserIdListToMap(List<Integer> createUserIdList);
-}
+//package com.xxl.job.executor.mvc.mapper;
+//
+//import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+//import com.xxl.job.executor.po.Env;
+//import org.jeecg.modules.tort.entity.FbmUserInfo;
+//import org.springframework.web.bind.annotation.Mapping;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+///**
+// * @Description: 用户作者
+// * @Author: jeecg-boot
+// * @Date: 2021-09-06
+// * @Version: V1.0
+// */
+//@Mapping
+//public interface EvnMapper extends BaseMapper<Env> {
+//
+//    Map<Integer, FbmUserInfo> getByUserIdListToMap(List<Integer> createUserIdList);
+//}
