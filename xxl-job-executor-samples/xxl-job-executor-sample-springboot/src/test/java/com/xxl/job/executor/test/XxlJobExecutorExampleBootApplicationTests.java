756c5415a8bee1d6b579bfb425265abade0425bc
kidoneself <8768611@qq.com>
2021-09-18 18:28:11 +0800

    init

--- xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/test/java/com/xxl/job/executor/test/XxlJobExecutorExampleBootApplicationTests.java
+++ xxl-job-executor-samples/xxl-job-executor-sample-springboot/src/test/java/com/xxl/job/executor/test/XxlJobExecutorExampleBootApplicationTests.java
@@ -3,9 +3,9 @@ package com.xxl.job.executor.test;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.xxl.job.executor.po.JxFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,9 +14,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.List;
+import java.util.Random;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -29,22 +28,50 @@ public class XxlJobExecutorExampleBootApplicationTests {
         System.err.println(formatStr);
 
         Unirest.setTimeouts(0, 0);
-        HttpResponse<String> response = Unirest.get("https://pro.m.jd.com/mall/active/2kmaPNrGDNYo1LKwYtRoaSmsgbj6/index.html?babelChannel=dongdongnongchang")
-                .header("user-agent", "jdapp;android;10.1.2;10;1393336683662616-3693934383367343;network/4g;model/JEF-AN00;addressid/4168482853;aid/193f8fbac9948c74;oaid/1cad52a0-a01e-4a4b-96ac-4a734a49e3e6;osVer/29;appBuild/89760;partner/huaweiharmony;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 10; JEF-AN00 Build/HUAWEIJEF-AN00; wv) AppleWebKit/537.36 (KHTML")
-                .header("accept", "*/*")
-                .header("Host", "api.m.jd.com")
-                .header("sec-fetch-site", "same-site")
-                .header("sec-fetch-mode", "no-cors")
-                .header("sec-fetch-dest", "script")
-                .header("referer", "https://carry.m.jd.com/babelDiy/Zeus/3KSjXqQabiTuD1cJ28QskrpWoBKT/index.html?babelChannel=121&lng=121.463611&lat=31.021696&sid=5ff1f498bb1025bac5c96263ecafc15w&un_area=2_2813_61130_0")
-                .header("accept-encoding", "gzip, deflate")
-                .header("accept-language", "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7")
-                .header("cookie", "pt_key=AAJhMQSFAEDpKABMNbc_OnIYLPXvxZJ742n5QPFXRtkm0c2Hi0DYHFN7VXDJX1IHNIITB-viic0sL5pk_UN3OUShqTOzCDxb;pt_pin=%E6%80%AA%E7%9B%97%E5%9F%BA%E5%BE%B78768611;")
+        HttpResponse<String> response = Unirest.get("https://wq.jd.com/user/info/QueryJDUserInfo?sceneval=2")
+                .header("user-agent", "jdpingou;android;5.5.0;10;193f8fbac9948c74;network/UNKNOWN;model/JEF-AN00;appBuild/18299;partner/huawei01;;session/73;aid/193f8fbac9948c74;oaid/1cad52a0-a01e-4a4b-96ac-4a734a49e3e6;pap/JA2019_3111789;brand/HUAWEI;eu/1393336683662616;fv/3693934383367343;Mozilla/5.0 (Linux; Android 10; JEF-AN00 Build/HUAWEIJEF-AN00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36")
+                .header("Accept","application/json,text/plain, */*" )
+                .header("Content-Type","application/x-www-form-urlencoded" )
+                .header("Accept-Encoding","gzip, deflate, br" )
+                .header("Accept-Language","zh-cn" )
+                .header("Connection","keep-alive" )
+                .header("Cookie","pt_key=AAJhQEfuADCnjcl3yHkyhCTSylvkHv4JlA62JrG3uOLxHgcIj6OCUgtfuH7dgZHPuDwuso3blO0;pt_pin=jd_6784f6c82972a;" )
+                .header("Referer","https://wqs.jd.com/my/jingdou/my.shtml?sceneval=2" )
                 .asString();
 
+        InputStream rawBody = response.getRawBody();
         String body = response.getBody();
+        JSONObject parse1 = JSONObject.parseObject(body);
+        int i = body.indexOf("window._CONFIG = ");
+        int i2 = body.indexOf(" ;var __getImgUrl");
+        String substring = body.substring(i + "window._CONFIG = ".length(), i2);
+        JSONArray parse = JSONArray.parseArray(substring);
 
-        System.out.println(response);
+        JSONArray skinConfig = parse.getJSONObject(0).getJSONArray("skinConfig");
+        List<JxFactory> jxFactories = skinConfig.toJavaList(JxFactory.class);
+        jxFactories.forEach(jxFactory -> {
+            if (jxFactory.getLink() != null/* && jxFactory.getStart() != null && jxFactory.getEnd() != null*/) {
+                System.out.println(jxFactory.getActiveId());
+                System.out.println(jxFactory.getLink());
+                System.out.println(jxFactory.getEnd());
+            }
+        });
+
+
     }
 
+    @Test
+    public void extracted() {
+        System.out.println(generateFp());
+    }
+
+    private long generateFp() {
+        while (true) {
+            long numb = (long) (Math.random() * 100000000 * 1000000); // had to use this as int's are to small for a 13 digit number.
+            if (String.valueOf(numb).length() == 13) {
+                System.out.println(numb);
+                return numb;
+            }
+        }
+    }
 }
