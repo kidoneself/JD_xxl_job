201deec8a7a43c2309fb5bb53e7884716faed8dc
kidoneself <47913364+kidoneself@users.noreply.github.com>
2021-10-02 09:21:00 +0800

    k

--- xxl-job-admin/src/main/java/com/xxl/job/admin/controller/IndexController.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/controller/IndexController.java
@@ -23,87 +23,101 @@ import java.util.Map;
 
 /**
  * index controller
+ *
  * @author xuxueli 2015-12-19 16:13:16
  */
 @Controller
 public class IndexController {
 
-	@Resource
-	private XxlJobService xxlJobService;
+    @Resource
+    private XxlJobService xxlJobService;
 
-	@RequestMapping("/")
-	public String index(Model model) {
+    @RequestMapping("/")
+    public String index(Model model) {
 
-		Map<String, Object> dashboardMap = xxlJobService.dashboardInfo();
-		model.addAllAttributes(dashboardMap);
+        Map<String, Object> dashboardMap = xxlJobService.dashboardInfo();
+        model.addAllAttributes(dashboardMap);
 
-		return "index";
-	}
+        return "index";
+    }
 
     @RequestMapping("/chartInfo")
-	@ResponseBody
-	public ReturnT<Map<String, Object>> chartInfo(Date startDate, Date endDate) {
+    @ResponseBody
+    public ReturnT<Map<String, Object>> chartInfo(Date startDate, Date endDate) {
         ReturnT<Map<String, Object>> chartInfo = xxlJobService.chartInfo(startDate, endDate);
         return chartInfo;
     }
-	
-	@RequestMapping("/toLogin")
-	@PermessionLimit(limit=false)
-	public String toLogin(Model model, HttpServletRequest request) {
-		if (PermissionInterceptor.ifLogin(request)) {
-			return "redirect:/";
-		}
-		return "login";
-	}
-	
-	@RequestMapping(value="login", method=RequestMethod.POST)
-	@ResponseBody
-	@PermessionLimit(limit=false)
-	public ReturnT<String> loginDo(HttpServletRequest request, HttpServletResponse response, String userName, String password, String ifRemember){
-		// valid
-		if (PermissionInterceptor.ifLogin(request)) {
-			return ReturnT.SUCCESS;
-		}
 
-		// param
-		if (userName==null || userName.trim().length()==0 || password==null || password.trim().length()==0){
-			return new ReturnT<String>(500, I18nUtil.getString("login_param_empty"));
-		}
-		boolean ifRem = ifRemember != null && ifRemember.trim().length() > 0 && "on".equals(ifRemember);
+    @RequestMapping("/toLogin")
+    @PermessionLimit(limit = false)
+    public String toLogin(Model model, HttpServletRequest request) {
+        if (PermissionInterceptor.ifLogin(request)) {
+            return "redirect:/";
+        }
+        return "login";
+    }
 
-		// do login
-		boolean loginRet = PermissionInterceptor.login(response, userName, password, ifRem);
-		if (!loginRet) {
-			return new ReturnT<>(500, I18nUtil.getString("login_param_unvalid"));
-		}
-		return ReturnT.SUCCESS;
-	}
-	
-	@RequestMapping(value="logout", method=RequestMethod.POST)
-	@ResponseBody
-	@PermessionLimit(limit=false)
-	public ReturnT<String> logout(HttpServletRequest request, HttpServletResponse response){
-		if (PermissionInterceptor.ifLogin(request)) {
-			PermissionInterceptor.logout(request, response);
-		}
-		return ReturnT.SUCCESS;
-	}
-	
-	@RequestMapping("/help")
-	public String help() {
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    @ResponseBody
+    @PermessionLimit(limit = false)
+    public ReturnT<String> loginDo(HttpServletRequest request, HttpServletResponse response, String userName, String password, String ifRemember) {
+        // valid
+        if (PermissionInterceptor.ifLogin(request)) {
+            return ReturnT.SUCCESS;
+        }
+
+        // param
+        if (userName == null || userName.trim().length() == 0 || password == null || password.trim().length() == 0) {
+            return new ReturnT<String>(500, I18nUtil.getString("login_param_empty"));
+        }
+        boolean ifRem = ifRemember != null && ifRemember.trim().length() > 0 && "on".equals(ifRemember);
+
+        // do login
+        boolean loginRet = PermissionInterceptor.login(response, userName, password, ifRem);
+        if (!loginRet) {
+            return new ReturnT<>(500, I18nUtil.getString("login_param_unvalid"));
+        }
+        return ReturnT.SUCCESS;
+    }
+
+
+    @RequestMapping(value = "/admin/login", method = RequestMethod.POST)
+    @ResponseBody
+    @PermessionLimit(limit = false)
+    public ReturnT<String> login(String userName, String password) {
+        if (userName == null || userName.trim().length() == 0 || password == null || password.trim().length() == 0) {
+            return new ReturnT<String>(500, I18nUtil.getString("login_param_empty"));
+        }
+        // do login
+
+        return ReturnT.SUCCESS;
+    }
+
+    @RequestMapping(value = "logout", method = RequestMethod.POST)
+    @ResponseBody
+    @PermessionLimit(limit = false)
+    public ReturnT<String> logout(HttpServletRequest request, HttpServletResponse response) {
+        if (PermissionInterceptor.ifLogin(request)) {
+            PermissionInterceptor.logout(request, response);
+        }
+        return ReturnT.SUCCESS;
+    }
+
+    @RequestMapping("/help")
+    public String help() {
 
 		/*if (!PermissionInterceptor.ifLogin(request)) {
 			return "redirect:/toLogin";
 		}*/
 
-		return "help";
-	}
+        return "help";
+    }
 
-	@InitBinder
-	public void initBinder(WebDataBinder binder) {
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		dateFormat.setLenient(false);
-		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
-	}
-	
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        dateFormat.setLenient(false);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+    }
+
 }
